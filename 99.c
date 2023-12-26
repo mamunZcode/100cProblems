d3cac59908ded72408795c4aafbf5bd5591c91a1
Md.Muntasir Mamun <132230558+mamunZcode@users.noreply.github.com>
2023-12-26 23:37:03 +0600

    99.c

---
+++ 99.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    double result;
+    scanf("%d",&number);
+
+    result = sqrt(number);
+    int final_result=result+.5;
+    printf("Rsult is: %d",final_result);
+
+
+return 0;
+}
